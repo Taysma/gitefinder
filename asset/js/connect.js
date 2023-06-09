c5f17927cb3238ad8b4143ea4d84cf7b217f40af
Taysma <walesca.telemaq@gmail.com>
2023-06-09 10:56:04 +0200

    maj connect

---
+++ asset/js/connect.js
@@ -0,0 +1,5 @@
+// Slide page de connection
+
+document.querySelector('.img__btn').addEventListener('click', function () {
+    document.querySelector('.cont').classList.toggle('s--signup');
+    });
