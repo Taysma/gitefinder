5943c5068f1ff2c60c6df6ad85a17a16f9d59127
orikham <orikham@hotmail.fr>
2023-06-12 11:53:27 +0200

    poursuite du CRUDdes model

--- model/CategoryModel.php
+++ model/CategoryModel.php
@@ -1,29 +1,56 @@
 <?php
-class CategoryModel extends Model {
-    
-    public function getAllCategory(){
+class CategoryModel extends Model
+{
+
+    public function createCategory(Category $category)
+    {
+        $id_category = $category->getId_category();
+        $tag = $category->getTag();
+        $name = $category->getName();
+        $slug = $category->getSlug();
+
+
+
+
+        $req = $this->getDb()->prepare("INSERT INTO `category` (`id_category`, `tag`, `name`, `slug`) VALUES (:id_category, :tag, :name, :slug)");
+        $req->bindParam(":id_category", $id_category, PDO::PARAM_INT);
+        $req->bindParam(":tag", $tag, PDO::PARAM_STR);
+        $req->bindParam(":name", $name, PDO::PARAM_STR);
+        $req->bindParam(":slug", $slug, PDO::PARAM_STR);
+
+
+
+        $req->execute();
+
+
+    }
+
+    public function getAllCategory()
+    {
         $categories = [];
 
         $req = $this->getDb()->query('SELECT `id_category`, `tag`, `name`, `slug` FROM `category`');
 
-        while($category = $req->fetch(PDO::FETCH_ASSOC)){
+        while ($category = $req->fetch(PDO::FETCH_ASSOC)) {
             $categories[] = new Category($category);
         }
         return $categories;
     }
 
-    public function getOneCategory(int $id_category){
+    public function getOneCategory(int $id_category)
+    {
 
         $req = $this->getDb()->prepare('SELECT `id_category`, `tag`, `name`, `slug` FROM `category` WHERE `id_category` = :id_category');
-        $req->bindParam('id_category',$id_category,PDO::PARAM_INT);
+        $req->bindParam('id_category', $id_category, PDO::PARAM_INT);
         $req->execute();
 
         $category = new Category($req->fetch(PDO::FETCH_ASSOC));
 
         return $category;
     }
-    
-    public function getrentalsByCategory(int $id_category) {
+
+    public function getrentalsByCategory(int $id_category)
+    {
         $rentals = [];
 
         $req = $this->getDb()->prepare('SELECT `rental`.`id_rental`, `rental`.`id_user`, `rental`.`title`, `rental`.`capacity`, `rental`.`surface_area`, `rental`.`city`, `rental`.`address`, `rental`.`content` 
@@ -41,5 +68,50 @@ class CategoryModel extends Model {
         }
 
         return $rentals;
+    }
+
+
+    public function update(Category $category)
+    {
+        $id_category = $category->getId_category();
+        $tag = $category->getTag();
+        $name = $category->getName();
+        $slug = $category->getSlug();
+
+
+        $req = $this->getDb()->prepare("UPDATE `category` SET `id_category`=':id_category',`tag`=':tag',`name`=':name',`slug`=':slug' ");
+        $req->bindParam(":id_category", $id_category, PDO::PARAM_INT);
+        $req->bindParam(":tag", $tag, PDO::PARAM_STR);
+        $req->bindParam(":name", $name, PDO::PARAM_STR);
+        $req->bindParam(":slug", $slug, PDO::PARAM_STR);
+
+        $req->execute();
+    }
+
+    public function deleterental(int $id)
+    {
+        // Start a new transaction
+        $this->getDb()->beginTransaction();
+
+        try {
+            // Delete links in rental_category table
+            $req = $this->getDb()->prepare('DELETE FROM `rental_category` WHERE `id_category` = :id_category');
+            $req->bindParam('id', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete the category
+            $req = $this->getDb()->prepare('DELETE FROM `rental` WHERE `id_category` = :id_category');
+            $req->bindParam('id', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Commit the transaction if both operations succeeded
+            $this->getDb()->commit();
+
+        } catch (Exception $e) {
+            // If any operation fails, an exception is thrown
+            // Rollback the transaction
+            $this->getDb()->rollBack();
+            throw $e; // or handle it in another way depending on your needs
+        }
     }
 }
--- model/RentalModel.php
+++ model/RentalModel.php
@@ -1,34 +1,63 @@
 <?php
-class RentalModel extends Model {
+class RentalModel extends Model
+{
 
-    public function getLastTenPost(){
+    public function createRental(Rental $rental)
+    {
+        $id_rental = $rental->getId_rental();
+        $id_user = $rental->getId_user();
+        $title = $rental->getTitle();
+        $capacity = $rental->getCapacity();
+        $surface_area = $rental->getSurface_area();
+        $city = $rental->getCity();
+        $address = $rental->getAddress();
+        $content = $rental->getContent();
+
+        $req = $this->getDb()->prepare('INSERT INTO `id_rental`, `id_user`, `title`, `capacity`, `surface_area`, `content`, `city`, `address`, `content` ) VALUES (:id_rental, :id_user, :title, :capacity, :surface_area, :city, :address, :content )');
+
+        $req->bindParam('id_rental', $id_rental, PDO::PARAM_INT);
+        $req->bindParam('id_user', $id_user, PDO::PARAM_INT);
+        $req->bindParam('title', $title, PDO::PARAM_STR);
+        $req->bindParam('capacity', $capacity, PDO::PARAM_INT);
+        $req->bindParam('surface_area', $surface_area, PDO::PARAM_INT);
+        $req->bindParam('city', $city, PDO::PARAM_STR);
+        $req->bindParam('address', $address, PDO::PARAM_STR);
+        $req->bindParam('content', $content, PDO::PARAM_STR);
+
+        $req->execute();
+    }
+
+    public function getLastTenRental()
+    {
         $rentals = [];
 
         $req = $this->getDb()->query('SELECT `id_rental`, `id_user`, `title`, `capacity`, `surface_area`, `content`, `city`, `address`, `content` FROM `rental` ORDER BY `id` DESC LIMIT 50');
 
-        while($rental = $req->fetch(PDO::FETCH_ASSOC)){
+        while ($rental = $req->fetch(PDO::FETCH_ASSOC)) {
             $rentals[] = new Rental($rental);
         }
 
         return $rentals;
     }
 
-    public function getAllrentals(){
+    public function getAllrentals()
+    {
         $rentals = [];
 
         $req = $this->getDb()->query('SELECT `id_rental`, `id_user`, `title`, `capacity`, `surface_area`, `content`, `city`, `address`, `content` FROM `rental` ORDER BY `id_rental` DESC');
 
-        while($rental = $req->fetch(PDO::FETCH_ASSOC)){
+        while ($rental = $req->fetch(PDO::FETCH_ASSOC)) {
             $rentals[] = new Rental($rental);
         }
 
         return $rentals;
     }
 
-    public function getOnerental(int $id_rental){
+    public function getOnerental(int $id_rental)
+    {
 
         $req = $this->getDb()->prepare('SELECT `id_rental`, `id_user`, `title`, `capacity`, `surface_area`, `content`, `city`, `address`, `content` FROM `rental` WHERE `id_rental`= :id');
-        $req->bindParam('id_rental',$id_rental,PDO::PARAM_INT);
+        $req->bindParam('id_rental', $id_rental, PDO::PARAM_INT);
         $req->execute();
 
         $rental = new Rental($req->fetch(PDO::FETCH_ASSOC));
@@ -36,7 +65,8 @@ class RentalModel extends Model {
         return $rental;
     }
 
-    public function getUserrentals(int $id_user){
+    public function getUserrentals(int $id_user)
+    {
         $rentals = [];
 
         $req = $this->getDb()->prepare('SELECT `rental`.`id_rental`, `rental`.`id_user`, `rental`.`title`, `rental`.`capacity`, `rental`.`surface_area`, `rental`.`city`, `rental`.`address`, `rental`.`content`, `user`.`id_user`, `user`.`firstname`, `user`.`lastname`, `user`.`mail`, `user`.`birthdate`, `user`.`password`, `user`.`content`, `user`.`roles`
@@ -51,80 +81,86 @@ class RentalModel extends Model {
             $rentals[] = new Rental($rentalData);
         }
 
-        $req->closeCursor();
+
         return $rentals;
     }
 
-    public function addrental (Rental $rental){
+
+
+    public function updateRental(rental $rental)
+    {
         $id_rental = $rental->getId_rental();
-        $id_user = $rental->getId_user();
         $title = $rental->getTitle();
         $capacity = $rental->getCapacity();
         $surface_area = $rental->getSurface_area();
         $city = $rental->getCity();
         $address = $rental->getAddress();
         $content = $rental->getContent();
-        
-        $req = $this->getDb()->prepare('INSERT INTO `id_rental`, `id_user`, `title`, `capacity`, `surface_area`, `content`, `city`, `address`, `content` ) VALUES (:id_rental, :id_user, :title, :capacity, :surface_area, :city, :address, :content )');
 
-        $req->bindParam('id_rental', $id_rental, PDO::PARAM_INT);
-        $req->bindParam('id_user', $id_user, PDO::PARAM_INT);        
-        $req->bindParam('title', $title, PDO::PARAM_STR);
-        $req->bindParam('capacity', $capacity, PDO::PARAM_INT);
-        $req->bindParam('surface_area', $surface_area, PDO::PARAM_INT);        
-        $req->bindParam('city', $city, PDO::PARAM_STR);
-        $req->bindParam('address', $address, PDO::PARAM_STR);
-        $req->bindParam('content', $content, PDO::PARAM_STR);
+        $req = $this->getDb()->prepare('UPDATE `rental` SET `title` = :title, `capacity` = :capacity, `surface_area` = :surface_area, `city` = :city, `address` = :address,`content` = :content WHERE `id_rental` = :id');
+
+        $req->bindParam(':id_rental', $id_rental, PDO::PARAM_INT);
+        $req->bindParam(':title', $title, PDO::PARAM_STR);
+        $req->bindParam(':capacity', $capacity, PDO::PARAM_STR);
+        $req->bindParam(':surface_area', $surface_area, PDO::PARAM_STR);
+        $req->bindParam(':city', $city, PDO::PARAM_STR);
+        $req->bindParam(':address', $address, PDO::PARAM_STR);
+        $req->bindParam(':content', $content, PDO::PARAM_STR);
+
 
         $req->execute();
     }
 
-    public function editrental(rental $rental) {
-        $id_rental = $rental->getId_rental();
-        $title = $rental->getTitle();
-        $capacity = $rental->getCapacity();
-        $surface_area = $rental->getSurface_area();
-        $city = $rental->getCity();
-        $address = $rental->getAddress();
-        $content = $rental->getContent();
-    
-        $req = $this->getDb()->prepare('UPDATE `rental` SET `title` = :title, `capacity` = :capacity, `surface_area` = :surface_area, `city` = :city, `address` = :address,`content` = :content WHERE `id_rental` = :id');
-    
-        $req->bindParam('id_rental', $id_rental, PDO::PARAM_INT);
-        $req->bindParam('title', $title, PDO::PARAM_STR);
-        $req->bindParam('capacity', $capacity, PDO::PARAM_STR);
-        $req->bindParam('surface_area', $surface_area, PDO::PARAM_STR);
-        $req->bindParam('city', $city, PDO::PARAM_STR);
-        $req->bindParam('address', $address, PDO::PARAM_STR);
-        $req->bindParam('content', $content, PDO::PARAM_STR);
-        
-    
-        $req->execute();
-    }    
+    public function deleterental(int $id)
+    {
+        // Start a new transaction
+        $this->getDb()->beginTransaction();
 
-    public function deleterental(int $id){
-            // Start a new transaction
-            $this->getDb()->beginTransaction();
-            
-            try {
-                // Delete links in rental_category table
-                $req = $this->getDb()->prepare('DELETE FROM `rental_category` WHERE `id_rental` = :id');
-                $req->bindParam('id', $id, PDO::PARAM_INT);
-                $req->execute();
-        
-                // Delete the rental
-                $req = $this->getDb()->prepare('DELETE FROM `rental` WHERE `id_rental` = :id');
-                $req->bindParam('id', $id, PDO::PARAM_INT);
-                $req->execute();
-        
-                // Commit the transaction if both operations succeeded
-                $this->getDb()->commit();
-        
-            } catch(Exception $e) {
-                // If any operation fails, an exception is thrown
-                // Rollback the transaction
-                $this->getDb()->rollBack();
-                throw $e;  // or handle it in another way depending on your needs
-            }
+        try {
+
+            // Delete links in chat table
+            $req = $this->getDb()->prepare('DELETE FROM `chat` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete links in picture table
+            $req = $this->getDb()->prepare('DELETE FROM `picture` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete links in reservation table
+            $req = $this->getDb()->prepare('DELETE FROM `reservation` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete links in review table
+            $req = $this->getDb()->prepare('DELETE FROM `review` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete links in wishlist table
+            $req = $this->getDb()->prepare('DELETE FROM `wishlist` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete links in rental_category table
+            $req = $this->getDb()->prepare('DELETE FROM `rental_category` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Delete the rental
+            $req = $this->getDb()->prepare('DELETE FROM `rental` WHERE `id_rental` = :id_rental');
+            $req->bindParam('id_rental', $id, PDO::PARAM_INT);
+            $req->execute();
+
+            // Commit the transaction if both operations succeeded
+            $this->getDb()->commit();
+
+        } catch (Exception $e) {
+            // If any operation fails, an exception is thrown
+            // Rollback the transaction
+            $this->getDb()->rollBack();
+            throw $e; // or handle it in another way depending on your needs
+        }
     }
 }
