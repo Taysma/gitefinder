ef014381c0eed10d59485dd59ff681c5b06aa42c
DayZmooN <karim-du01@live.fr>
2023-08-16 09:43:21 +0200

    post profil reservationModel pictur clalend slider

---
+++ asset/js/profil-picture.js
@@ -0,0 +1,39 @@
+// image profil
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez l'élément de la photo de profil et l'input de téléchargement de fichier
+    const profilePicture = document.getElementById('profile-picture');
+    const profileUpload = document.getElementById('profile-upload');
+
+    // Fonction pour mettre à jour l'image de profil
+    function updatePicture(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            profilePicture.style.backgroundImage = `url(${reader.result})`;
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
+    // Ajoutez un événement de changement de fichier à l'input de téléchargement
+    profileUpload.addEventListener('change', updatePicture);
+});
+
+// edit profil
+document.addEventListener("DOMContentLoaded", function () {
+    let btnIconEdit = document.getElementById('edit-icon');
+    let containerProfil = document.getElementsByClassName('profil-information')[0];
+    let containerInformationProfil = document.getElementsByClassName('name-lastname-email-number')[0];
+    let containerTitle = document.getElementsByClassName('title-img-btn')[0]; // Correction ici
+
+    btnIconEdit.addEventListener('click', function () {
+        containerProfil.style.display = "block";
+        containerInformationProfil.style.display = "none";
+        containerTitle.style = "margin:auto;";
+    });
+})
