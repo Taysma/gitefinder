7d4e889620f9318e3de0edc746d7560dbd127137
DayZmooN <karim-du01@live.fr>
2023-08-20 21:23:26 +0200

    update js and css and template

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,209 +1,280 @@
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let buttonSearch = document.querySelector('.button-search');
+//     let searchBar = document.querySelector('.search-bar-form');
+//     let menuBurger = document.querySelectorAll('.menu-burger-header');
+//     let isSearchBarVisible = false;
+//     let isAboveThreshold = window.innerWidth > 769;
+
+//     buttonSearch.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         isSearchBarVisible = !isSearchBarVisible;
+
+//         if (isSearchBarVisible) {
+//             searchBar.style.display = 'flex';
+//             searchBar.animate(
+//                 [
+//                     { opacity: 0, transform: 'translateY(-10px)' },
+//                     { opacity: 1, transform: 'translateY(0)' },
+//                 ],
+//                 {
+//                     duration: 500,
+//                     easing: 'ease-out',
+//                     fill: 'both',
+//                 }
+//             );
+//             buttonSearch.innerHTML =
+//                 '<img src="/projets/gitefinder/asset/media/icon/x-solid.svg" alt="">';
+//             menuBurger.style.display = 'none';
+//         } else {
+//             searchBar.animate(
+//                 [
+//                     { opacity: 1, transform: 'translateY(0)' },
+//                     { opacity: 0, transform: 'translateY(-10px)' },
+//                 ],
+//                 {
+//                     duration: 500,
+//                     easing: 'ease-out',
+//                     fill: 'both',
+//                 }
+//             );
+
+//             setTimeout(function () {
+//                 searchBar.style.display = 'none';
+//                 buttonSearch.innerHTML =
+//                     '<img src="/projets/gitefinder/asset/media/icon/magnifying-glass-solid.svg" alt="">';
+//                 menuBurger.style.display = 'block';
+//             }, 500);
+//         }
+//     });
+
+//     window.addEventListener('resize', function () {
+//         const currentWidth = window.innerWidth;
+//         const isThresholdExceeded = currentWidth == 769;
+
+//         if (isThresholdExceeded !== isAboveThreshold) {
+//             isAboveThreshold = isThresholdExceeded;
+//             if (isAboveThreshold) {
+//                 location.reload(); // Actualiser la page sans recharger
+//             }
+//         }
+//     });
+// });
+
+
+
+// //menu burger
+// let btnBurger = document.querySelector('.menu-burger-header');
+// let menuModal = document.getElementById('burger-menu-modal');
+// let menuOpen = false; // Booléen pour garder une trace de l'état du menu
+
+// if (btnBurger && menuModal) {
+//     btnBurger.addEventListener('click', function (event) {
+//         event.stopPropagation();
+//         if (menuOpen) {
+//             menuModal.style.display = 'none';
+//             menuOpen = false;
+//         } else {
+//             menuModal.style.display = 'flex';
+//             menuOpen = true;
+//         }
+//     });
+
+//     document.addEventListener('click', function () {
+//         if (menuOpen) {
+//             menuModal.style.display = 'none';
+//             menuOpen = false;
+//         }
+//     });
+// }
+
+
+
+// // slider image post
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var sliderImages = document.getElementsByClassName('sliderImage');
+//     var mainImage = document.getElementById('mainImage');
+
+//     for (var i = 0; i < sliderImages.length; i++) {
+//         sliderImages[i].addEventListener('click', function () {
+//             var tempSrc = mainImage.src;  // Stocker temporairement l'image principale
+//             mainImage.src = this.src;  // Changer l'image principale
+//             this.src = tempSrc;  // Changer l'image du slider
+//         });
+//     }
+// });
+
+
+// // image profil
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let hearts = document.querySelectorAll('.heart-kr');
+
+//     hearts.forEach(heart => {
+//         setupHeart(heart);
+//     });
+
+//     function setupHeart(heart) {
+//         let isFilled = false;
+
+//         heart.addEventListener('click', function () {
+//             isFilled = !isFilled;
+//             animateHeart(heart, isFilled);
+//         });
+
+//         function animateHeart(heartElement, isFilled) {
+//             let totalFrames = 28;
+//             let frames = Math.floor(totalFrames / 2);
+//             let interval = 1000 / frames;
+
+//             let currentFrame = 0;
+//             let position = isFilled ? -currentFrame * 100 : 0;
+
+//             let animation = setInterval(function () {
+//                 heartElement.style.backgroundPosition = `${position}px 0`;
+//                 currentFrame++;
+
+//                 if (currentFrame >= frames) {
+//                     clearInterval(animation);
+//                 }
+
+//                 position = isFilled ? -currentFrame * 100 : 0;
+//             }, interval);
+//         }
+//     }
+// });
+
+
 document.addEventListener("DOMContentLoaded", function () {
+    handleSearchBar();
+    handleBurgerMenu();
+    handleSliderImages();
+    handleHeartAnimation();
+});
+
+function handleSearchBar() {
     let buttonSearch = document.querySelector('.button-search');
     let searchBar = document.querySelector('.search-bar-form');
-    let menuBurger = document.querySelectorAll('.menu-burger-header');
+    let menuBurger = document.querySelector('.menu-burger-header');
     let isSearchBarVisible = false;
-    let isAboveThreshold = window.innerWidth > 769;
 
-    buttonSearch.addEventListener('click', function (event) {
-        event.stopPropagation();
-        isSearchBarVisible = !isSearchBarVisible;
+    if (buttonSearch && searchBar && menuBurger) {
+        buttonSearch.addEventListener('click', function (event) {
+            event.stopPropagation();
+            isSearchBarVisible = !isSearchBarVisible;
 
-        if (isSearchBarVisible) {
-            searchBar.style.display = 'flex';
-            searchBar.animate(
-                [
-                    { opacity: 0, transform: 'translateY(-10px)' },
-                    { opacity: 1, transform: 'translateY(0)' },
-                ],
-                {
-                    duration: 500,
-                    easing: 'ease-out',
-                    fill: 'both',
-                }
-            );
-            buttonSearch.innerHTML =
-                '<img src="/projets/gitefinder/asset/media/icon/x-solid.svg" alt="">';
-            menuBurger.style.display = 'none';
-        } else {
-            searchBar.animate(
-                [
-                    { opacity: 1, transform: 'translateY(0)' },
-                    { opacity: 0, transform: 'translateY(-10px)' },
-                ],
-                {
-                    duration: 500,
-                    easing: 'ease-out',
-                    fill: 'both',
-                }
-            );
-
-            setTimeout(function () {
-                searchBar.style.display = 'none';
-                buttonSearch.innerHTML =
-                    '<img src="/projets/gitefinder/asset/media/icon/magnifying-glass-solid.svg" alt="">';
-                menuBurger.style.display = 'block';
-            }, 500);
-        }
-    });
-
-    window.addEventListener('resize', function () {
-        const currentWidth = window.innerWidth;
-        const isThresholdExceeded = currentWidth == 769;
-
-        if (isThresholdExceeded !== isAboveThreshold) {
-            isAboveThreshold = isThresholdExceeded;
-            if (isAboveThreshold) {
-                location.reload(); // Actualiser la page sans recharger
+            if (isSearchBarVisible) {
+                displaySearchBar(searchBar, buttonSearch, menuBurger);
+            } else {
+                hideSearchBar(searchBar, buttonSearch, menuBurger);
             }
-        }
-    });
-});
-
-
-
-//menu burger
-let btnBurger = document.querySelector('.menu-burger-header');
-let menuModal = document.getElementById('burger-menu-modal');
-let menuOpen = false; // Booléen pour garder une trace de l'état du menu
-
-if (btnBurger && menuModal) {
-    btnBurger.addEventListener('click', function (event) {
-        event.stopPropagation();
-        if (menuOpen) {
-            menuModal.style.display = 'none';
-            menuOpen = false;
-        } else {
-            menuModal.style.display = 'flex';
-            menuOpen = true;
-        }
-    });
-
-    document.addEventListener('click', function () {
-        if (menuOpen) {
-            menuModal.style.display = 'none';
-            menuOpen = false;
-        }
-    });
-}
-
-
-
-// slider image post
-
-document.addEventListener('DOMContentLoaded', function () {
-    var sliderImages = document.getElementsByClassName('sliderImage');
-    var mainImage = document.getElementById('mainImage');
-
-    for (var i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].addEventListener('click', function () {
-            var tempSrc = mainImage.src;  // Stocker temporairement l'image principale
-            mainImage.src = this.src;  // Changer l'image principale
-            this.src = tempSrc;  // Changer l'image du slider
         });
     }
-});
+}
 
+function displaySearchBar(searchBar, buttonSearch, menuBurger) {
+    searchBar.style.display = 'flex';
+    animateElement(searchBar, [
+        { opacity: 0, transform: 'translateY(-10px)' },
+        { opacity: 1, transform: 'translateY(0)' }
+    ]);
+    buttonSearch.innerHTML = '<img src="/projets/gitefinder/asset/media/icon/x-solid.svg" alt="">';
+    menuBurger.style.display = 'none';
+}
 
-// image profil
+function hideSearchBar(searchBar, buttonSearch, menuBurger) {
+    animateElement(searchBar, [
+        { opacity: 1, transform: 'translateY(0)' },
+        { opacity: 0, transform: 'translateY(-10px)' }
+    ]);
+    setTimeout(function () {
+        searchBar.style.display = 'none';
+        buttonSearch.innerHTML = '<img src="/projets/gitefinder/asset/media/icon/magnifying-glass-solid.svg" alt="">';
+        menuBurger.style.display = 'block';
+    }, 500);
+}
 
+function animateElement(element, keyframes, options = { duration: 500, easing: 'ease-out', fill: 'both' }) {
+    element.animate(keyframes, options);
+}
 
+function handleBurgerMenu() {
+    let btnBurger = document.querySelector('.menu-burger-header');
+    let menuModal = document.getElementById('burger-menu-modal');
+    let menuOpen = false;
 
+    if (btnBurger && menuModal) {
+        btnBurger.addEventListener('click', function (event) {
+            event.stopPropagation();
+            menuOpen = !menuOpen;
+            menuModal.style.display = menuOpen ? 'flex' : 'none';
+        });
 
-
-
-
-function copyToClipboard(text) {
-    lettextarea = document.createElement("textarea");
-    textarea.textContent = text;
-    textarea.style.position = "fixed";
-    document.body.appendChild(textarea);
-    textarea.select();
-    try {
-        document.execCommand("copy");
-    } catch (ex) {
-        console.warn("Copy to clipboard failed.", ex);
-        return false;
-    } finally {
-        document.body.removeChild(textarea);
+        document.addEventListener('click', function () {
+            if (menuOpen) {
+                menuModal.style.display = 'none';
+                menuOpen = false;
+            }
+        });
     }
 }
 
-function shareOnFacebook() {
-    const currentURL = window.location.href;
-    const fbShareURL = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(currentURL);
-    window.open(fbShareURL, '_blank');
+function handleSliderImages() {
+    let sliderImages = document.querySelectorAll('.sliderImage');
+    let mainImage = document.getElementById('mainImage');
+
+    if (mainImage) {
+        sliderImages.forEach(sliderImage => {
+            sliderImage.addEventListener('click', function () {
+                let tempSrc = mainImage.src;
+                mainImage.src = this.src;
+                this.src = tempSrc;
+            });
+        });
+    }
 }
 
-function shareOnTwitter() {
-    const currentURL = window.location.href;
-    const twitterShareURL = "https://twitter.com/share?url=" + encodeURIComponent(currentURL);
-    window.open(twitterShareURL, '_blank');
-}
-
-function shareOnWhatsApp() {
-    const currentURL = window.location.href;
-    const whatsappShareURL = "https://wa.me/?text=" + encodeURIComponent(currentURL);
-    window.open(whatsappShareURL, '_blank');
-}
-
-function shareOnInstagram() {
-    // Discord n'a pas de lien de partage direct, donc nous copierons simplement l'URL pour l'utilisateur
-    const currentURL = window.location.href;
-    copyToClipboard(currentURL);
-    alert("Lien copié dans le presse-papiers. Collez-le dans Discord pour le partager !");
-    // const currentURL = window.location.href;
-    // const twitterShareURL = "https://www.instagram.com/share?url=" + encodeURIComponent(currentURL);
-    // window.open(twitterShareURL, '_blank');
-}
-
-
-
-// Attacher les écouteurs d'événements aux boutons
-document.getElementById('facebookShareButton').addEventListener('click', shareOnFacebook);
-document.getElementById('twitterShareButton').addEventListener('click', shareOnTwitter);
-document.getElementById('whatsappShareButton').addEventListener('click', shareOnWhatsApp);
-document.getElementById('instagramShareButton').addEventListener('click', shareOnInstagram);
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
+function handleHeartAnimation() {
     let hearts = document.querySelectorAll('.heart-kr');
 
     hearts.forEach(heart => {
-        setupHeart(heart);
-    });
-
-    function setupHeart(heart) {
         let isFilled = false;
 
         heart.addEventListener('click', function () {
             isFilled = !isFilled;
             animateHeart(heart, isFilled);
         });
+    });
+}
 
-        function animateHeart(heartElement, isFilled) {
-            let totalFrames = 28;
-            let frames = Math.floor(totalFrames / 2);
-            let interval = 1000 / frames;
+function animateHeart(heartElement, isFilled) {
+    let totalFrames = 28;
+    let frames = Math.floor(totalFrames / 2);
+    let interval = 1000 / frames;
+    let currentFrame = 0;
+    let position = isFilled ? -currentFrame * 100 : 0;
 
-            let currentFrame = 0;
-            let position = isFilled ? -currentFrame * 100 : 0;
+    let animation = setInterval(function () {
+        heartElement.style.backgroundPosition = `${position}px 0`;
+        currentFrame++;
 
-            let animation = setInterval(function () {
-                heartElement.style.backgroundPosition = `${position}px 0`;
-                currentFrame++;
-
-                if (currentFrame >= frames) {
-                    clearInterval(animation);
-                }
-
-                position = isFilled ? -currentFrame * 100 : 0;
-            }, interval);
+        if (currentFrame >= frames) {
+            clearInterval(animation);
         }
-    }
-});
+
+        position = isFilled ? -currentFrame * 100 : 0;
+    }, interval);
+}
--- asset/js/rentalMaps.js
+++ asset/js/rentalMaps.js
@@ -1,65 +1,111 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var mapElement = document.getElementById('mapid');
-    var latitude = mapElement.dataset.latitude;
-    var longitude = mapElement.dataset.longitude;
+// document.addEventListener('DOMContentLoaded', function () {
+//     var mapElement = document.getElementById('mapid');
+//     var latitude = mapElement.dataset.latitude;
+//     var longitude = mapElement.dataset.longitude;
 
-    var map = L.map('mapid').setView([latitude, longitude], 17);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom: 19,
-    }).addTo(map);
+//     var map = L.map('mapid').setView([latitude, longitude], 17);
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//         maxZoom: 19,
+//     }).addTo(map);
 
-    var myIcon = L.icon({
-        iconUrl: "../asset/media/icon/home.svg",
-        iconSize: [38, 95],      // Taille de l'icône
-        // iconAnchor: [22, 94],    // Point de l'icône qui correspondra à la position du marqueur
-        // popupAnchor: [-3, -76],  // Point à partir duquel le popup s'ouvrira, par rapport à l'iconAnchor
-    });
+//     var myIcon = L.icon({
+//         iconUrl: "../asset/media/icon/home.svg",
+//         iconSize: [38, 95],      // Taille de l'icône
+//         // iconAnchor: [22, 94],    // Point de l'icône qui correspondra à la position du marqueur
+//         // popupAnchor: [-3, -76],  // Point à partir duquel le popup s'ouvrira, par rapport à l'iconAnchor
+//     });
 
-    L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
-});
+//     L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+// });
+
+// // document.addEventListener('DOMContentLoaded', function () {
+// //     let inputSearchElements = document.querySelectorAll('.name'); // J'ai changé l'ID en classe
+// //     let h1Elements = document.querySelectorAll('.h1-complete');  // J'ai également changé l'ID en classe
+
+// //     inputSearchElements.forEach((inputSearch, index) => {
+// //         let originalH1Content = h1Elements[index].innerHTML;
+
+// //         inputSearch.addEventListener('input', function () {
+// //             if (inputSearch.value.length > 2) {
+// //                 // Ajoutez la valeur de l'input à la suite du contenu original de h1
+// //                 h1Elements[index].innerHTML = originalH1Content + inputSearch.value;
+// //             } else {
+// //                 // Réinitialisez à la valeur originale si la longueur du texte est inférieure à 3
+// //                 h1Elements[index].innerHTML = originalH1Content;
+// //             }
+// //         });
+// //     });
+// // });
+
+
+
+// // document.addEventListener('DOMContentLoaded', function () {
+// //     let inputNames = document.querySelectorAll('.name');
+// //     let h1Elements = document.querySelectorAll('.h1-complete-edit');
+
+// //     inputNames.forEach((inputName) => {
+// //         inputName.addEventListener('input', function () {
+// //             h1Elements.forEach((h1Element) => {
+// //                 h1Element.innerHTML = inputName.value;
+// //             });
+// //         });
+// //     });
+// // });
 
 // document.addEventListener('DOMContentLoaded', function () {
-//     let inputSearchElements = document.querySelectorAll('.name'); // J'ai changé l'ID en classe
-//     let h1Elements = document.querySelectorAll('.h1-complete');  // J'ai également changé l'ID en classe
+//     let inputName = document.querySelector('.name input'); // Sélection de l'input avec la classe 'name'
+//     let h1Element = document.querySelector('.h1-complete-edit'); // Sélection du h1 avec la classe 'h1-complete-edit'
 
-//     inputSearchElements.forEach((inputSearch, index) => {
-//         let originalH1Content = h1Elements[index].innerHTML;
-
-//         inputSearch.addEventListener('input', function () {
-//             if (inputSearch.value.length > 2) {
-//                 // Ajoutez la valeur de l'input à la suite du contenu original de h1
-//                 h1Elements[index].innerHTML = originalH1Content + inputSearch.value;
-//             } else {
-//                 // Réinitialisez à la valeur originale si la longueur du texte est inférieure à 3
-//                 h1Elements[index].innerHTML = originalH1Content;
-//             }
-//         });
+//     inputName.addEventListener('input', function () {
+//         h1Element.innerHTML = inputName.value;
 //     });
 // });
 
 
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     let inputNames = document.querySelectorAll('.name');
-//     let h1Elements = document.querySelectorAll('.h1-complete-edit');
-
-//     inputNames.forEach((inputName) => {
-//         inputName.addEventListener('input', function () {
-//             h1Elements.forEach((h1Element) => {
-//                 h1Element.innerHTML = inputName.value;
-//             });
-//         });
-//     });
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
-    let inputName = document.querySelector('.name input'); // Sélection de l'input avec la classe 'name'
-    let h1Element = document.querySelector('.h1-complete-edit'); // Sélection du h1 avec la classe 'h1-complete-edit'
-
-    inputName.addEventListener('input', function () {
-        h1Element.innerHTML = inputName.value;
-    });
+    initMap();
+    handleNameInputChange();
 });
 
+function initMap() {
+    const mapElement = document.getElementById('mapid');
+    let map;
 
+    if (mapElement) {
+        const latitude = mapElement.dataset.latitude;
+        const longitude = mapElement.dataset.longitude;
+
+        if (!window.mapInstance) {
+            map = L.map('mapid').setView([latitude, longitude], 17);
+            window.mapInstance = map;  // Store the map instance globally
+        } else {
+            map = window.mapInstance;
+        }
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+            maxZoom: 19,
+        }).addTo(map);
+
+        const myIcon = L.icon({
+            iconUrl: "../asset/media/icon/home.svg",
+            iconSize: [38, 95]
+        });
+
+        L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+    }
+}
+
+function handleNameInputChange() {
+    const inputName = document.querySelector('.name input');
+    const h1Element = document.querySelector('.h1-complete-edit');
+
+    if (inputName && h1Element) {
+        inputName.addEventListener('input', function () {
+            h1Element.innerHTML = inputName.value;
+        });
+    }
+}
